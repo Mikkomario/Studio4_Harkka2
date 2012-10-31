91800d4b0010be0e97c032a6af919933e9da6161
Mikkomario <mikko_hilpinen@hotmail.com>
2012-10-31 10:36:12 +0200

    Modified the flower_power.png and created a phaase class (still empty)

---
+++ src/studio4harkka2/Phase.java
@@ -0,0 +1,13 @@
+package studio4harkka2;
+
+/**
+ * This class represents a single phase in the program's flow
+ * Each phase creates a few placers
+ *
+ * @author Gandalf.
+ *         Created 31.10.2012.
+ */
+public abstract class Phase
+{
+
+}
