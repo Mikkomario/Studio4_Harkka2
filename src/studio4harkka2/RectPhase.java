385ec1371f2a047bc2bfcbd68d1dfc43620f8d3f
Tiitu Lahti <tiitu.lahti@aalto.fi>
2012-11-01 17:44:41 +0200

    modified rectangle details and visuals of the phase-classes

--- src/studio4harkka2/RectPhase.java
+++ src/studio4harkka2/RectPhase.java
@@ -2,15 +2,22 @@ package studio4harkka2;
 
 public class RectPhase extends Phase{
 
-	
+	/**
+	 * Constructor
+	 * 
+	 * @param parentApplet defines the parent applet of this phase
+	 */
 	public RectPhase(Studio4Harkka2 parentApplet){
 		super(parentApplet, 4000);
 	}
 
+	/**
+	 * Sets the visuals of the applet and adds a rectangle placer
+	 */
 	@Override
 	public void onStart() {
 		
-		getApplet().setVisuals(0, 10, 80, 10, 0.05);
+		getApplet().setVisuals(0, 15, 95, 20, 0.05);
 		
 		this.addPlacer(new RectPlacer(this.getApplet()));
 		
--- src/studio4harkka2/RectPlacer.java
+++ src/studio4harkka2/RectPlacer.java
@@ -8,6 +8,11 @@ import processing.core.PApplet;
 public class RectPlacer extends Placer {
 	private Random rand;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param parentApplet defines the parent applet of the placer
+	 */
 	public RectPlacer(PApplet parentApplet){
 		super(parentApplet);
 		this.rand = new Random();
@@ -16,6 +21,10 @@ public class RectPlacer extends Placer {
 		}
 	}
 
+	/**
+	 * Method, that creates a new random rectangle with a probability of 70 %
+	 * @return a new rectangle or null
+	 */
 	@Override
 	public Particle generateParticle() {
 		if(this.rand.nextDouble() > 0.3){
@@ -24,7 +33,7 @@ public class RectPlacer extends Placer {
 			double maxVelocity = 8*this.rand.nextDouble();
 			double xscale = 5*this.rand.nextDouble();
 			double yscale = 5*this.rand.nextDouble();
-			int strokeWeight = this.rand.nextInt(10);
+			int strokeWeight = 2+this.rand.nextInt(8);
 			
 			return new Rectangle(x, y, maxVelocity, xscale, yscale, this, 
 					this.getApplet(), strokeWeight);
@@ -33,6 +42,10 @@ public class RectPlacer extends Placer {
 		return null;
 	}
 
+	/**
+	 * Goes through all the rectangles of this placer and calls their moving
+	 * method if they are moving 
+	 */
 	@Override
 	public void onStep() {
 		
